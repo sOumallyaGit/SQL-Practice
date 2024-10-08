52b5a2700cd92f97a521ee11291a04626e3c79c0
Soumallya Chandra Chandra <119125509+sOumallyaGit@users.noreply.github.com>
2024-10-08 18:27:26 +0530

    Add files via upload

---
+++ SQL Class 4.sql
@@ -0,0 +1,17 @@
+SELECT FirstName FROM EmployeeRecords WHERE EmployeeID = 1;
+
+SELECT FirstName,LastName FROM EmployeeRecords WHERE EmployeeID = 1;
+
+SELECT * FROM EmployeeRecords
+
+SELECT FirstName,LastName FROM EmployeeRecords WHERE Salary = 62000;
+SELECT FirstName,LastName FROM EmployeeRecords WHERE Salary >= 45000;
+SELECT FirstName,Salary FROM EmployeeRecords WHERE Salary >= 60000;
+SELECT FirstName,Salary FROM EmployeeRecords WHERE Salary <= 60000;
+SELECT DISTINCT FirstName,LastName FROM EmployeeRecords WHERE Salary >= 45000;
+
+
+
+
+
+
