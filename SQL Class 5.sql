222d8ca2082776073b96ed307fa89a1b9363cd55
Soumallya Chandra Chandra <119125509+sOumallyaGit@users.noreply.github.com>
2024-10-08 18:43:33 +0530

    Add files via upload

---
+++ SQL Class 5.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Employees
+
+SELECT * FROM Employees ORDER BY Salary;
+
+SELECT * FROM Employees ORDER BY Salary desc;
+
+SELECT * FROM Employees ORDER BY EmployeeID desc;
+
+SELECT * FROM Employees ORDER BY FirstName asc , Salary desc;
+
+SELECT * FROM Employees ORDER BY Department asc , Salary desc;
