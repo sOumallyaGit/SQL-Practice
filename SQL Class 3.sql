7df88212abb1dff752eb3bd065241ce34565a4b8
Soumallya Chandra Chandra <119125509+sOumallyaGit@users.noreply.github.com>
2024-10-08 18:11:49 +0530

    Add files via upload

---
+++ SQL Class 3.sql
@@ -0,0 +1,22 @@
+USE employee_details
+
+SELECT * INTO #temp1
+FROM [dbo].[Employees]
+
+SELECT * FROM #temp1
+
+SELECT * FROM dbo.Employees
+
+SELECT * INTO #temp2
+FROM [dbo].[EmployeeRecords]
+
+
+SELECT * FROM #temp2
+
+SELECT * FROM [dbo].[EmployeeRecords]
+
+SELECT * INTO ##temp2
+FROM [dbo].[Employees]
+
+
+
